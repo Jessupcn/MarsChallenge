564aa00433b63992473c2203af4025a1b6cd5173
Connor Jessup <cjessup@umich.edu>
2018-11-05 22:27:55 -0500

    maintenance issue to get running

--- Server/index.js
+++ Server/index.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 
 // sends index.html
 app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+  res.sendFile(path.join(__dirname, '..', 'Client/index.html'));
 });
 
 // error handling endware
