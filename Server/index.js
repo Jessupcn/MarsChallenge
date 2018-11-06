cf3a0ec7fcf98bc12e022e7c58346ff8ceba74ea
Connor Jessup <cjessup@umich.edu>
2018-11-05 22:34:22 -0500

    edits

--- Server/index.js
+++ Server/index.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 
 // sends index.html
 app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'Client/index.html'));
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'));
 });
 
 // error handling endware
