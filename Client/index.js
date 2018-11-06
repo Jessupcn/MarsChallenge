d8f63be2af4d6069b4a6e5cdce7b59e0a27466b1
Connor Jessup <cjessup@umich.edu>
2018-11-05 22:23:02 -0500

    fixed package

--- Client/index.js
+++ Client/index.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { Router } from 'react-router-dom';
-import history from './history';
-import store from './redux_store';
 import App from './app';
 
 // give App a redux store and router
-ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('app'));
